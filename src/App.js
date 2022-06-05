f6dc4617973c8c204925a214c2362538c4d56b6d
yuhengwu <799810767@qq.com>
2022-06-06 01:07:08 +0800

    Implement slider and water level

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
+import Slider from '@material-ui/core/Slider';
+import {useState} from 'react';
+import Background from './background';
+import useWindowSize from './getSize';
+
 
 function App() {
+  const [prob, setProb] = useState(50);
+  const size = useWindowSize();
+
+  const handleSlider = (event, value) => {
+    setProb(value);
+  }
+
+  const sliderLen = Math.min(300, size.width*0.8);
+
   return (
     <div className="App">
+      <Background prob={prob}/>
+
+      <div className="slider" style={{
+        width: sliderLen + 'px',
+        left: (size.width-sliderLen)/2 + 'px'
+      }}>
+        <Slider
+          value={prob}
+          onChange={handleSlider}
+          min={0}
+          max={100}
+          step={1}
+          valueLabelDisplay="on"
+        />
+      </div>
+
     </div>
   );
 }
---
+++ src/background/index.js
@@ -0,0 +1,31 @@
+import './background.css';
+import Wave from 'react-wavify';
+import useWindowSize from '../getSize';
+
+function Background(props) {
+    const {prob} = props;
+
+    const ratio = Math.min(100, Math.max(0, prob/100));
+    const amplitude = ratio*(1-ratio)*4*50;
+    
+    const size = useWindowSize();
+
+    return (
+        <div className="background" style={{width: size.width, height: size.height}}>
+            <Wave fill="rgb(197, 184, 202)"
+                options={{
+                    height: size.height*(1-ratio)-amplitude/2,
+                    amplitude: amplitude,
+                    speed: 0.28,
+                    points: 3
+                }}
+                style={{
+                    height: '100%',
+                }}>
+            </Wave>
+        </div>
+    )
+
+}
+
+export default Background;
